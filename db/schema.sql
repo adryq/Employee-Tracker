070eea76006efe3278664abd16c391a2992ec1c0
Adry Quezada <adryq95@gmail.com>
2022-07-17 17:22:08 -0400

    seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,26 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    roles_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE
+);
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,23 @@ INSERT INTO department (name) VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal');
+
+INSERT INTO roles (title, salary, department_id) VALUES
+    ('Salesperson', 200000, 1),
+    ('Sales Closer', 70000, 1),
+    ('Junior Engineer', 100000, 2),
+    ('Lead Engineer', 250000, 2),
+    ('Accountant', 150000, 3),
+    ('Account Manager', 155000, 3),
+    ('Lawyer', 200000, 4),
+    ('Legal Team Lead', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES
+    ('Sara', 'White', 1, NULL),
+    ('Emily', 'Cortez', 2, NULL),
+    ('Michael', 'Chan', 3, NULL),
+    ('Ruida', 'Albakry', 4, NULL),
+    ('Adry', 'Castro', 5, NULL),
+    ('Ana', 'Rodriguez', 6, 1),
+    ('Johana', 'Calderon', 7, NULL),
+    ('Alex', 'Bello', 8, NULL);
